cce6ddb48454e6af2e01d85fb4fc6d70d380df35
gridialves <gridialves@gmail.com>
2019-07-07 17:33:56 -0300

    adicionar tradutor

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    var x = 
+    document.getElementById("google_translate_element");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }  
+    }
