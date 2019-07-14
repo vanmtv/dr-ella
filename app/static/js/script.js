839e28199d2b0a0a47d8f6301d52ec750df5e050
Vanessa <vanmtv@hotmail.com>
2019-07-14 08:43:29 -0300

    Deixar tradutor como inicial

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,16 @@
+// function mostrar() {
+//     let x = document.querySelector("#google_translate_element");
+//     if (x.style.display == "none") {
+//       x.style.display = "flex";
+//     } else {
+//       x.style.display = "none";
+//     }  
+// }
+
 function mostrar() {
-    let x = document.querySelector("#google_translate_element");
-    if (x.style.display == "none") {
+  let x = 
+  document.getElementById("google_translate_element");
+    if (x.style.display === "none") {
       x.style.display = "flex";
     } else {
       x.style.display = "none";
